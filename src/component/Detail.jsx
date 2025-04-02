70b63bacb251e5904358ab19a1209db7d419df1b
KikoLaw18 <wingnanglaw@gmail.com>
2025-04-02 09:31:01 -0700

    add image to assets

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
 const Detail = ({
-  descriptionText = { title: "", subtitle: "", body: "" },
-
+    descriptionText = { title: "", subtitle: "", body: "" },
 }) => {
 
 
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center text-center px-8 lg:px-12 gap-8">
-      {/* Text content section */}
-      <div className="w-full md:w-1/2 sm:w-100 px-4">
-        <h3 className="text-4xl font-moon">{descriptionText.title}</h3>
-        <h2 className="text-xl font-bold">{descriptionText.subtitle}</h2>
-        <p className="pt-4">{descriptionText.body}</p>
+    <>
+      <div className="pt-12 px-4 place-content-center w-100">
+        <h2 className="font-bold text-2xl">Grounds for Coffee:</h2>
+        <h1 className="font-bold text-2xl">UX/UI Redesign Case Study</h1>
+        <p className="pt-4">This project improves the Grounds for Coffee app by fixing navigation issues, refining layouts, and enhancing the user experience. The goal is to create a more intuitive and seamless ordering process.</p>
+      
+        <div className="bg-[#FFF6E3] font-bold rounded-4xl w-32 h-9 text-center content-center mt-4">UX/UI Design</div>
       </div>
-
-    </div>
+      <img src="" alt="" />
+    </>
+    
   );
 };
 
